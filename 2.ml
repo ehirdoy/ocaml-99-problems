a62b6541d33f384f7c68a64ecf1a920b90167978
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2018-10-09 09:57:25 +0900

    2: initial submit

---
+++ 2.ml
@@ -0,0 +1,12 @@
+(*
+	2. Find the last but one (last and penultimate) elements of a list. (easy)
+*)
+
+let rec last_two = function
+  | [] -> None | x :: [] -> None
+  | x :: y :: [] -> Some (x, y)
+  | x :: y :: xs -> last_two xs
+
+let test = last_two [] = None
+let test = last_two ["a"] = None
+let test = last_two ["a";"b";"c";"d"] = Some ("c", "d")
