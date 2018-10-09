df265590437492f5d3f6ed9a893657aa5a0d4e5c
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2018-10-09 12:08:53 +0900

    8: polished

--- 8.ml
+++ 8.ml
@@ -1,9 +1,8 @@
 (* 8. Eliminate consecutive duplicates of list elements. (medium) *)
 
 let rec compress = function
-  | [] -> []
-  | x::[] -> [x]
-  | x::x'::xs -> if x = x' then compress (x::xs) else x::compress (x'::xs)
+  | x::(x'::_ as t) -> if x = x' then compress t else x::compress t
+  | smaller -> smaller
 
 let test =
   compress ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"] =
