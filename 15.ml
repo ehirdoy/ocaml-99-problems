18da8fa95d63cdc9e1615ccf6c30d5554c4d9a78
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2018-10-10 11:43:52 +0900

    15: with prepend, more natural

--- 15.ml
+++ 15.ml
@@ -1,12 +1,13 @@
 (* 15. Replicate the elements of a list a given number of times. (medium) *)
 
 let replicate lst num =
-  let rec aux count acc = function
+  let rec prepend n acc x =
+    if n = 0 then acc else prepend (n - 1) (x::acc) x in
+  let rec aux acc = function
     | [] -> acc
-    | x::xs ->
-      if count = 1 then aux num (x::acc) xs else aux (count-1) (x::acc) (x::xs)
+    | x::xs -> aux (prepend num acc x) xs
   in
-  aux num [] (List.rev lst)
+  aux [] (List.rev lst)
 
 let test = replicate [] 3 = []
 let test = replicate ["a"] 2 = ["a";"a"]
