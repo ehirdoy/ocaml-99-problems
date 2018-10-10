f8651036a731887e06ef35e238b9503d1f92e806
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2018-10-10 12:55:00 +0900

    16: fold_left + rev

---
+++ 16.ml
@@ -0,0 +1,13 @@
+(* 16. Drop every N'th element from a list. (medium) *)
+
+let drop lst num =
+  let f (idx, acc) el =
+    if (idx mod num) = 0 then (idx+1, acc) else (idx+1, el::acc) in
+  match List.fold_left f (1, []) lst with
+  | _, acc -> List.rev acc
+
+let test1 = drop [] 3 = []
+let test2 = drop ["a"] 3 = ["a"]
+let test3 = drop ["a";"b";"c"] 3 = ["a";"b"]
+let test = drop ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j"] 3
+           = ["a"; "b"; "d"; "e"; "g"; "h"; "j"]
