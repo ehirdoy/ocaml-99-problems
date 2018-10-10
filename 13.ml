eb65fc0c5e3cd1a7f8bdbab6bd601ad4de4e4a73
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2018-10-10 10:25:21 +0900

    13: polished, in-place mutation

--- 13.ml
+++ 13.ml
@@ -15,16 +15,16 @@ type 'a rle =
   | Many of int * 'a
 
 let encode lst =
+  let rle count x = if count = 0 then One x else Many ((count + 1), x) in
   let rec aux count acc = function
     | [] -> acc
-    | [x] -> (count+1, x)::acc
+    | [x] -> (rle count x)::acc
     | x::(x'::_ as t) ->
       if x = x' then aux (count+1) acc t
-      else aux 0 ((count+1, x)::acc) t
+      else aux 0 ((rle count x)::acc) t
   in
   aux 0 [] lst
   |> List.rev
-  |> List.map (fun (n, x) -> if n = 1 then One x else Many (n, x))
 
 let test = encode [] = []
 let test = encode ["a"] = [One "a"]
