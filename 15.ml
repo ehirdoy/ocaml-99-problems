07bf8706e0814b848da35168b53b7942342d9292
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2018-10-10 11:19:01 +0900

    15: initial submit

---
+++ 15.ml
@@ -0,0 +1,16 @@
+(* 15. Replicate the elements of a list a given number of times. (medium) *)
+
+let replicate lst num =
+  let rec aux count acc = function
+    | [] -> acc
+    | [x] ->
+      if count = 1 then x::acc else aux (count-1) (x::acc) [x]
+    | x::xs ->
+      if count = 1 then aux num (x::acc) xs else aux (count-1) (x::acc) (x::xs)
+  in
+  List.rev (aux num [] lst)
+
+let test = replicate [] 3 = []
+let test = replicate ["a"] 2 = ["a";"a"]
+let test = replicate ["a";"b";"c"] 3
+           = ["a"; "a"; "a"; "b"; "b"; "b"; "c"; "c"; "c"]
