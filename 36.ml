0462322f62bf128e46ad0014a0f7ee133ce5ead8
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2018-10-14 08:19:55 +0900

    36: match, answer copied

--- 36.ml
+++ 36.ml
@@ -4,22 +4,17 @@
  * Hint: The problem is similar to problem Run-length encoding of a list
  * (direct solution). *)
 
-let rle list =
-  List.fold_left
-    (fun acc el ->
-       if List.mem_assoc el acc
-       then (el, (List.assoc el acc) + 1) :: (List.remove_assoc el acc)
-       else  (el, 1) :: acc)
-    [] list
-
 let factors n =
+  (* prime factors of n, starting d *)
   let rec aux d n =
     if n = 1 then [] else
-    if n mod d = 0 then d :: aux d (n / d) else aux (d+1) n
+    if n mod d = 0 then
+      match aux d (n / d) with
+      | (x, n) :: xs when x = d -> (x, n+1) :: xs
+      | xs -> (d, 1) :: xs
+    else aux (d+1) n
   in
   aux 2 n
-  |> List.sort (fun e1 e2 -> e2 - e1)
-  |> rle
 
 let test = factors 315
 (* = [(3, 2); (5, 1); (7, 1)] *)
