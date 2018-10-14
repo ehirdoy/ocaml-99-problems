1950740b2c42c121bb7e1fed966473be130444ae
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2018-10-15 00:59:02 +0900

    41: FIXME: stack overflow

--- 41.ml
+++ 41.ml
@@ -13,21 +13,16 @@ let rec seq a b =
   let a = (if a mod 2 = 0 then a else a+1) in
   if a > b then [] else (a :: (seq (a+2) b))
 
-let goldbach_list a b =
-  seq a b
-  |> List.rev
-  |> List.fold_left (fun acc el -> (el, goldbach el) :: acc) []
+let rec goldbach_list a b =
+  if a mod 2 = 1 then goldbach_list (a+1) b
+  else (a, (goldbach a)) :: (goldbach_list (a+2) b)
 
 let test = goldbach_list 9 20
 = [(10, (3, 7)); (12, (5, 7)); (14, (3, 11)); (16, (3, 13)); (18, (5, 13));
    (20, (3, 17))]
 
 let goldbach_limit a b c =
-  seq a b
-  |> List.rev
-  |> List.fold_left (fun acc el ->
-      let ((n1, n2) as v) = goldbach el in
-      if n1 > c && n2 > c then (el, v) :: acc else acc) []
+  goldbach_list a b |> List.filter (fun (_, (x, y)) -> x > c && y > c)
 
 let test = goldbach_limit 1 2000 50
 = [(992, (73, 919)); (1382, (61, 1321)); (1856, (67, 1789));
