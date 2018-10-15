a2c2c48a39e4d03b1d175c233c1a29ce360cb1ab
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2018-10-15 14:49:25 +0900

    49: copied answer
    No idea???

--- 49.ml
+++ 49.ml
@@ -23,3 +23,24 @@ let test = gray 2
 = ["00"; "01"; "11"; "10"]
 let test = gray 3
 = ["000"; "001"; "011"; "010"; "110"; "111"; "101"; "100"]
+
+
+(* ans *)
+let gray n =
+  let rec gray_next_level k l =
+    if k < n then
+      (* This is the core part of the Gray code construction.
+       * first_half is reversed and has a "0" attached to every element.
+       * Second part is reversed (it must be reversed for correct gray code).
+       * Every element has "1" attached to the front.*)
+      let (first_half, second_half) =
+        List.fold_left (fun (acc1, acc2) x ->
+            (("0"^x)::acc1, ("1"^x)::acc2 )) ([], []) l
+      in
+      (* List.rev_append turns first_half around and attaches it to second_half.
+       * The result is the modified first_half in correct order attached to
+       * the second_half modified in reversed order.*)
+      gray_next_level (k+1) (List.rev_append first_half second_half)
+    else l
+  in
+  gray_next_level 1 ["0"; "1"];;
