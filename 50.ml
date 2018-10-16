f2707971454442419de96d6ebb67954afb19e664
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2018-10-16 09:06:50 +0900

    50: add freq count in tree

--- 50.ml
+++ 50.ml
@@ -45,27 +45,28 @@ module Pq = struct
 end
 
 type tree =
-  | Leaf of string
-  | Node of tree * tree
+  | Leaf of string * int
+  | Node of tree * tree * int
 
 let rec huffman_tree q =
   match Pq.get_min q, Pq.get_min q with
-  | Some(p1, t1), Some(p2, t2) -> Pq.add q (p1 + p2) (Node(t1, t2));
+  | Some(p1, t1), Some(p2, t2) ->
+    Pq.add q (p1 + p2) (Node(t1, t2, p1 + p2));
     huffman_tree q
   | Some(_, t), None | None, Some(_, t) -> t
   | None, None -> assert false
 
 (* Build the prefix-free binary code from the tree *)
 let rec prefixes_of_tree prefix = function
-  | Leaf s -> [(s, prefix)]
-  | Node(t0, t1) -> prefixes_of_tree (prefix ^ "0") t0
+  | Leaf(s, _) -> [(s, prefix)]
+  | Node(t0, t1, _) -> prefixes_of_tree (prefix ^ "0") t0
                     @ prefixes_of_tree (prefix ^ "1") t1
 
 let huffman fs =
   if List.fold_left (fun s (_,p) -> s + p) 0 fs != 100 then
     failwith "huffman: sum of weights must be 100";
   let q = Pq.make() in
-  List.iter (fun (s, f) -> Pq.add q f (Leaf s)) fs;
+  List.iter (fun (s, f) -> Pq.add q f (Leaf (s, f))) fs;
   prefixes_of_tree "" (huffman_tree q)
 
 let test = huffman fs
