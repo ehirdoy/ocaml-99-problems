95e2d55a7c79c090f63f13424232cc524bc4c926
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2019-06-21 01:24:14 +0300

    simplified 19

--- 19.ml
+++ 19.ml
@@ -1,30 +1,19 @@
 (* 19. Rotate a list N places to the left. (medium) *)
 
-let split lst nr =
-  let rec aux n acc = function
-    | [] -> (List.rev acc, [])
-    | x::xs as l ->
-      if n = 0 then (List.rev acc, l) else aux (n-1) (x::acc) xs
-  in
-  let update n =
-    if n >= 0 then n else n + (List.length lst) in
+let rec rotate list nr = match list, nr with
+  | [], _ -> []
+  | list, nr when nr=0 -> list
+  | list, nr when nr<0 -> rotate list (List.length list + nr)
+  | x::xs, nr (*when nr>0*) -> rotate (xs @ [x]) (nr-1)
 
-  aux (update nr) [] lst
 
-let rotate lst nr = match lst with
-  | [] -> []
-  | [x] -> [x]
-  | x::xs ->
-    let (h, t) = split lst nr in
-    t @ h
-
-let test = rotate [] 3 = []
-let test = rotate ["a"] 1 = ["a"]
-let test = rotate ["a";"b"] 1 = ["b";"a"]
-let test = rotate ["a";"b"] 2 = ["a";"b"]
-let test =
+let test01 = rotate [] 3 = []
+let test02 = rotate ["a"] 1 = ["a"]
+let test03 = rotate ["a";"b"] 1 = ["b";"a"]
+let test04 = rotate ["a";"b"] 2 = ["a";"b"]
+let test05 =
   rotate ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"] 3
        = ["d"; "e"; "f"; "g"; "h"; "a"; "b"; "c"]
-let test =
+let test06 =
   rotate ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"] (-2)
        = ["g"; "h"; "a"; "b"; "c"; "d"; "e"; "f"]
