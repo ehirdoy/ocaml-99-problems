a0af7e281ed2b6090be9926719e406d288ed2d91
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2019-06-21 10:24:58 +0300

    Simplified 23 with List.fold

--- 23.ml
+++ 23.ml
@@ -4,35 +4,23 @@
  * We use the Random module but do not initialize it with Random.self_init
  * for reproducibility. *)
 
-exception Shouldnt_happen
+let rec print_intlist = function
+  | [] -> Printf.printf "\n"
+  | x::xs -> Printf.printf "%d " x; print_intlist xs
 
-let random_intlist n max =
-  let n = if n > max then max else n in
-  let rec aux n acc max =
-    if n < 0 then raise Shouldnt_happen
-    else if n = 0 then acc
-    else
-      let num = Random.int max in
-      let rec is_overlap nr = function
-        | [] -> false
-        | x::xs -> if nr = x then true else is_overlap nr xs in
-      if is_overlap num acc then aux n acc max
-      else aux (n-1) (num::acc) max in
-  aux n [] max |> List.sort (fun x y -> x - y)
+let rand_select list nr =
+  if List.length list <= nr then list else
+    let rec aux acc =
+      if List.length acc >= nr then acc else
+        let x = Random.int (List.length list) in
+        if List.mem x acc then aux acc else aux (x::acc)
+    in
+    let index = aux [] |> List.sort (fun a b -> b - a) in
+    print_intlist index;
+    List.fold_left (fun acc el -> (List.nth list el)::acc) [] index
 
-(* let test = random_intlist 3 8
- * let test = random_intlist 2 2
- * let test = random_intlist 2 9
- * let test = random_intlist 3 5 *)
-
-let rand_select lst nr =
-  let rec aux l = function
-    | [] -> []
-    | x::xs -> (List.nth l x) :: (aux l xs)
-  in
-  aux lst (random_intlist nr (List.length lst))
 
 let test1 = rand_select ["a";"b";"c";"d";"e";"f";"g";"h"] 3
-let test2 = rand_select ["a";"b"] 3 = ["a";"b"]
-let test3 = rand_select ["a"] 3 = ["a"]
+let test2 = rand_select ["a";"b"] 3
+let test3 = rand_select ["a"] 3
 let test4 = rand_select [] 3 = []
