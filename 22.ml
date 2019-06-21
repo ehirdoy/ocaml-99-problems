3fcb995e7fae2194c2a94d28919356a061310bf2
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2019-06-21 04:43:26 +0300

    update 22 with List.init

--- 22.ml
+++ 22.ml
@@ -2,14 +2,14 @@
  *
  * If first argument is greater than second, produce a list in decreasing order. *)
 
-let range s e =
-  let rec aux high low =
-    if high > low then high :: aux (high - 1) low else [low]
-  in
-  if s < e then List.rev (aux e s) else aux s e
+let range start _end =
+  let len = abs (_end - start) + 1 in
+  let dir =
+    if start >= _end then -1 else 1 in
+  List.init len (fun index -> start + index * dir)
 
-let test = range 1 1 = [1]
-let test = range 4 9
-= [4; 5; 6; 7; 8; 9]
-let test = range 9 4
-= [9; 8; 7; 6; 5; 4]
+let test01 = range 1 1 = [1]
+let test02 = range 4 9
+             = [4; 5; 6; 7; 8; 9]
+let test03 = range 9 4
+             = [9; 8; 7; 6; 5; 4]
