8bb70f2b6460015305d6370cbea5147d159fea8b
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2019-06-21 11:30:22 +0300

    simplified 25 with random repetetition of swap

--- 25.ml
+++ 25.ml
@@ -1,26 +1,22 @@
 (* 25. Generate a random permutation of the elements of a list. (easy) *)
 
-let random_intlist n max =
-  let n = if n > max then max else n in
-  let rec aux n acc max =
-    if n < 0 then raise Shouldnt_happen
-    else if n = 0 then acc
-    else
-      let num = Random.int max in
-      let rec is_overlap nr = function
-        | [] -> false
-        | x::xs -> if nr = x then true else is_overlap nr xs in
-      if is_overlap num acc then aux n acc max
-      else aux (n-1) (num::acc) max in
-  aux n [] max
+#use "24.ml"
 
-let permutation lst =
-  let len = List.length lst in
-  let index = random_intlist len len in
-  let rec aux index lst = match index with
-    | [] -> []
-    | x::xs -> (List.nth lst x) :: (aux xs lst) in
-  aux index lst
+let permutation list =
+  let rec aux acc m =
+    if m = 0 then acc else
+      let swap l n1 n2 =
+        let ar = Array.of_list l in
+        let temp = ar.(n1) in
+        ar.(n1) <- ar.(n2);
+        ar.(n2) <- temp;
+        Array.to_list ar
+      in
+      let len = List.length list in
+      let n1, n2 = Random.(int len, int len) in
+      aux (swap acc n1 n2) (m-1)
+  in
+  aux list (Random.int 9)
 
 let test = permutation ["a"; "b"; "c"; "d"; "e"; "f"]
                   (* = ["a"; "e"; "f"; "b"; "d"; "c"] *)
