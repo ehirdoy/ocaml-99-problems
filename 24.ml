358baaa3523e6a7ab496f451e036487946de1232
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2019-06-21 11:00:28 +0300

    simplified 24 with List.mem

--- 24.ml
+++ 24.ml
@@ -2,23 +2,15 @@
  *
  * The selected numbers shall be returned in a list. *)
 
-exception Shouldnt_happen
-
-let random_intlist n max =
-  let n = if n > max then max else n in
-  let rec aux n acc max =
-    if n < 0 then raise Shouldnt_happen
-    else if n = 0 then acc
-    else
-      let num = Random.int max in
-      let rec is_overlap nr = function
-        | [] -> false
-        | x::xs -> if nr = x then true else is_overlap nr xs in
-      if is_overlap num acc then aux n acc max
-      else aux (n-1) (num::acc) max in
-  aux n [] max |> List.sort (fun x y -> x - y)
-
-let lotto_select n max = random_intlist n (max+1)
+let lotto_select n max =
+  let rec aux acc = function
+    | 0 -> acc
+    | n ->
+      let x = Random.int (max+1) in
+      if List.mem x acc then aux acc n else
+        aux (x::acc) (n-1)
+  in
+  aux [] n |> List.sort (fun a b -> a - b)
 
 let test = lotto_select 6 49
 let test = lotto_select 0 49 = []
